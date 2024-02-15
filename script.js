a1806e76565a4b039ab345d7573b631feb1dcfb9
Cecelia Braswell <ceceliabraswell@yahoo.com>
2024-02-14 21:38:33 -0800

    third commit

--- script.js
+++ script.js
@@ -10,5 +10,43 @@ function hideSidebar() {
 
 function sendEmail() {
   alert("Email sent!");
-  document.getElementById('reset').reset();
+  document.getElementById("reset").reset();
 }
+
+// JavaScript for gallery slider
+const initSlider = () => {
+  const imageList = document.querySelector(".slider-wrapper .image-list");
+  const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+  const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+  const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = imageList.clientWidth * direction;
+      imageList.scrollBy({ left: scrollAmount});
+    });
+  });
+
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display =
+      imageList.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display =
+      imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+
+  const updateScrollThumbPosition = () => {
+    const scrollPostion = imageList.scrollLeft;
+    const thumbPosition = (scrollPostion / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+    scrollbarThumb.style.left = `${thumbPosition}px`;
+  };
+
+  imageList.addEventListener("scroll", () => {
+    handleSlideButtons();
+    updateScrollThumbPosition();
+  });
+};
+
+window.addEventListener("load", initSlider);
