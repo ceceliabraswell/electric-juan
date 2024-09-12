24cfb3871e8aad3aa9c6de5c7552e11a500e2684
Cecelia Braswell <ceceliabraswell@yahoo.com>
2024-09-11 20:16:08 -0700

    Fixing responsiveness of navbar on home page

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
+
+  const homeResponsive = document.querySelector(".homeResponsive");
+  homeResponsive.style.display = "flex";
 }
 
 function hideSidebar() {
   const hideSidebar = document.querySelector(".sidebar");
   hideSidebar.style.display = "none";
+
+  const homeResponsive = document.querySelector(".homeResponsive");
+  homeResponsive.style.display = "none";
 }
 
 function sendEmail() {
