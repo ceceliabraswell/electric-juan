b8461a21b5833960682be4b111c68332057e18f1
Cecelia Braswell <ceceliabraswell@yahoo.com>
2025-04-28 21:46:40 -0700

    Deleted JS code
    Deleted obsolete JS code

--- script.js
+++ script.js
@@ -1,61 +1,7 @@
-// function showSidebar() {
-//   const sidebar = document.querySelector(".sidebar");
-//   sidebar.style.display = "flex";
-
-//   const homeResponsive = document.querySelector(".homeResponsive");
-//   homeResponsive.style.display = "flex";
-// }
-
-// function hideSidebar() {
-//   const hideSidebar = document.querySelector(".sidebar");
-//   hideSidebar.style.display = "none";
-
-//   const homeResponsive = document.querySelector(".homeResponsive");
-//   homeResponsive.style.display = "none";
-// }
-
 function sendEmail() {
   alert("Email sent!");
   document.getElementById("reset").reset();
 }
-
-// JavaScript for gallery slider
-// const initSlider = () => {
-//   const imageList = document.querySelector(".slider-wrapper .image-list");
-//   const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
-//   const sliderScrollbar = document.querySelector(".gallery-container .slider-scrollbar");
-//   const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
-//   const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
-
-
-//   slideButtons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//       const direction = button.id === "prev-slide" ? -1 : 1;
-//       const scrollAmount = imageList.clientWidth * direction;
-//       imageList.scrollBy({ left: scrollAmount});
-//     });
-//   });
-
-//   const handleSlideButtons = () => {
-//     slideButtons[0].style.display =
-//       imageList.scrollLeft <= 0 ? "none" : "block";
-//     slideButtons[1].style.display =
-//       imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
-//   };
-
-//   const updateScrollThumbPosition = () => {
-//     const scrollPostion = imageList.scrollLeft;
-//     const thumbPosition = (scrollPostion / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
-//     scrollbarThumb.style.left = `${thumbPosition}px`;
-//   };
-
-//   imageList.addEventListener("scroll", () => {
-//     handleSlideButtons();
-//     updateScrollThumbPosition();
-//   });
-// };
-
-// window.addEventListener("load", initSlider);
 
 const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slide');
@@ -109,11 +55,4 @@ function handleSwipeGesture() {
     if (swipeDistance < 0) nextSlide();
     else prevSlide();
   }
-}
-
-// responsive navigation
-
-function toggleMenu() {
-  const menu = document.querySelector('.home-responsive');
-  menu.classList.toggle('show');
 }
