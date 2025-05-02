99d20e0644c35be780ced720422f36e72adaee8f
Cecelia Braswell <ceceliabraswell@yahoo.com>
2025-05-01 21:34:32 -0700

    Update HTML
    Moved logo outside of nav list items

--- script.js
+++ script.js
@@ -56,3 +56,12 @@ function handleSwipeGesture() {
     else prevSlide();
   }
 }
+
+// toggle responsive navigation menu
+
+const hamburger = document.getElementById('.hamburger');
+const navMenu = document.getElementById('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+})
