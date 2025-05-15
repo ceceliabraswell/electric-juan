848fe76913211923a9d0b535d0599ef1d6d637c1
Cecelia Braswell <ceceliabraswell@yahoo.com>
2025-05-14 20:08:51 -0700

    Edited HTML
    Added HTML for gallery slider

--- script.js
+++ script.js
@@ -3,59 +3,63 @@ function sendEmail() {
   document.getElementById("reset").reset();
 }
 
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
 
-let currentIndex = 0;
-const visibleSlides = 4; // Change based on how many you want visible
-const totalSlides = slides.length;
 
-function updateSliderPosition() {
-  const slideWidth = slides[0].offsetWidth;
-  slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-}
+// old gallery script
 
-function nextSlide() {
-  if (currentIndex < totalSlides - visibleSlides) {
-    currentIndex++;
-    updateSliderPosition();
-  }
-}
+// const slider = document.querySelector('.slider');
+// const slides = document.querySelectorAll('.slide');
+// const nextBtn = document.querySelector('.next');
+// const prevBtn = document.querySelector('.prev');
 
-function prevSlide() {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateSliderPosition();
-  }
-}
+// let currentIndex = 0;
+// const visibleSlides = 4;  Change based on how many you want visible
+// const totalSlides = slides.length;
 
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
+// function updateSliderPosition() {
+//   const slideWidth = slides[0].offsetWidth;
+//   slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+// }
 
-// Swipe Support
-let touchStartX = 0;
-let touchEndX = 0;
+// function nextSlide() {
+//   if (currentIndex < totalSlides - visibleSlides) {
+//     currentIndex++;
+//     updateSliderPosition();
+//   }
+// }
 
-const sliderContainer = document.querySelector('.slider-container');
+// function prevSlide() {
+//   if (currentIndex > 0) {
+//     currentIndex--;
+//     updateSliderPosition();
+//   }
+// }
 
-sliderContainer.addEventListener('touchstart', (e) => {
-  touchStartX = e.changedTouches[0].screenX;
-}, false);
+// nextBtn.addEventListener('click', nextSlide);
+// prevBtn.addEventListener('click', prevSlide);
 
-sliderContainer.addEventListener('touchend', (e) => {
-  touchEndX = e.changedTouches[0].screenX;
-  handleSwipeGesture();
-}, false);
+//  Swipe Support
+// let touchStartX = 0;
+// let touchEndX = 0;
 
-function handleSwipeGesture() {
-  const swipeDistance = touchEndX - touchStartX;
-  if (Math.abs(swipeDistance) > 50) {
-    if (swipeDistance < 0) nextSlide();
-    else prevSlide();
-  }
-}
+// const sliderContainer = document.querySelector('.slider-container');
+
+// sliderContainer.addEventListener('touchstart', (e) => {
+//   touchStartX = e.changedTouches[0].screenX;
+// }, false);
+
+// sliderContainer.addEventListener('touchend', (e) => {
+//   touchEndX = e.changedTouches[0].screenX;
+//   handleSwipeGesture();
+// }, false);
+
+// function handleSwipeGesture() {
+//   const swipeDistance = touchEndX - touchStartX;
+//   if (Math.abs(swipeDistance) > 50) {
+//     if (swipeDistance < 0) nextSlide();
+//     else prevSlide();
+//   }
+// }
 
 // toggle responsive navigation menu
 
